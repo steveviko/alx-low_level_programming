f1b194cf271d9d85f3aaea982f992a1806f4305e
Steven Opio <stevenviko@gmail.com>
2022-10-13 06:58:09 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
+ * *main - prints the phrase 
  *
- *  *main - prints the phrase 
+ **Return: 0 if exited ,else non-zero 
  *
- *   *Return: 0 if exited ,else non-zero 
- *
- *    */
+ **/
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
