b61afc2cad9e6ba58263fefd384bfb0839aff3f1
Steven Opio <stevenviko@gmail.com>
2022-10-31 03:31:08 -0700

    Task 1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,30 +1,20 @@
 #include "main.h"
 
 /**
- *_strncat - concatenates two strings
- *@dest: destination
- *@src: source
- *@n: amount of bytes used from src
+ *_memcpy - copies memory area
+ *@dest: destination memory area
+ *@src: source memory area
+ *@n: bytes filled
  *Return: the pointer to dest
- **/
+**/
 
-
-char *_strncat(char *dest, char *src, int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int count = 0, count2 = 0;
+	unsigned int i;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
 
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
 	return (dest);
 }
+
