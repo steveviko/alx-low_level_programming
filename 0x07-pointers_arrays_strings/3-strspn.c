ede3495f4209d8442deb018a9f082853c088e124
Steven Opio <stevenviko@gmail.com>
2022-10-31 08:52:13 -0700

    Task 3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  *_strspn - gets the length of a prefix substring
  *@s: initial segment
  *@accept: accepted bytes
- *Return: the number of accepted bytes
+ *Return:the number of accepted bytes
  **/
 
 unsigned int _strspn(char *s, char *accept)
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		bool = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
-		{ 
+		{
 			if (*(s + i) == *(accept + j))
 			{
 				bool = 0;
